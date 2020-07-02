ef5bc3ba97a42318e4bfc656f3c8652829dbaa13
MoonSangJin <1128msj@naver.com>
2020-07-02 16:40:42 +0900

    second layout

---
+++ index.js
@@ -0,0 +1,20 @@
+var arrLang = {
+    'kor':
+    {
+        'home': 'korean'
+    },
+    'en':
+    {
+        'home': "눌렀을때"
+    }
+};
+
+$(function () {
+    $('.translate').click(function () {
+        var lang = $(this).attr('id');
+
+        $('.lang').each(function (index, element) {
+            $(this).text(arrLang[lang][$(this).attr('key')]);
+        });
+    });
+});
