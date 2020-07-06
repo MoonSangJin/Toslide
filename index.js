f2eb129e307b3d3c26ead8e0cf79b8a19f9e40ef
MoonSangJin <1128msj@naver.com>
2020-07-06 17:21:58 +0900

    kor-eng translate기능 추가, @gmail.com 앞 폰트 컬러 변경

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-/*var arrLang = {
-    'kor':
-    {
-        'home': 'korean'
-    },
-    'en':
-    {
-        'home': "눌렀을때"
+var change = 0; //0이면 영어 paragraph Kor button, 1이면 한국 paragraph Eng button
+var menubar = document.querySelectorAll(".nav-link");
+const KoreanMenu = new Array("청중 질의응답","온라인 강의","온라인 시험","사용 사례","연락처");
+const EngMenu = new Array("Audience Q&A", "Online Class","Online Test","Use Cases","Contact");
+
+function change_text() {
+    if (change == 0) {
+        document.querySelector('.translate-img').src="asset/img/eng.png"
+        document.querySelector('.lecturer').innerHTML = "강사로 참여";
+        document.querySelector('.audience').innerHTML = "청중으로 참여";
+        document.querySelector('.lecture-id').innerHTML = "강사아이디 : 강사의 Gmail/@gmail.com 의 앞부분";
+
+        document.querySelector('.OTUG').innerHTML = "온라인 시험 사용법";
+        document.querySelector('.AQUG').innerHTML = "청중 질의응답 사용법";
+        document.querySelector('.OCUG').innerHTML = "온라인 강의 사용법";
+
+        for(var i=0; i<menubar.length; i++){
+            menubar[i].innerHTML = KoreanMenu[i];
+        }
+        change = 1;
     }
-};
+    else {
+        document.querySelector('.translate-img').src="asset/img/kor.png"
+        document.querySelector('.lecturer').innerHTML = "Lecturer";
+        document.querySelector('.audience').innerHTML = "Audience";
+        document.querySelector('.lecture-id').innerHTML = "The lecture id : the part before @gmail.com of the lecturer.";
 
-$(function () {
-    $('.translate').click(function () {
-        var lang = $(this).attr('id');
+        document.querySelector('.OTUG').innerHTML = "Online Test User Guide";
+        document.querySelector('.AQUG').innerHTML = "Audience Q&A User Guide";
+        document.querySelector('.OCUG').innerHTML = "Online Class User Guide";
+        
+        for(var i=0; i<menubar.length; i++){
+            menubar[i].innerHTML = EngMenu[i];
+        }
+        change = 0;
+    }
 
-        $('.lang').each(function (index, element) {
-            $(this).text(arrLang[lang][$(this).attr('key')]);
-        });
-    });
-});*/
-
-$('.right1').click(function(){
-    alert('clicked');
-});
+}
