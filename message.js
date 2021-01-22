e3bc347fbdff9bc4e5d17b3bf7be79a1493b6d8c
MoonSangJin <1128msj@naver.com>
2021-01-22 14:28:03 +0900

    feat/keywordRow

--- message.js
+++ message.js
@@ -1,8 +1,15 @@
 const openButton = document.getElementById('open');
 const modal = document.querySelector('.modal');
 const overlay = modal.querySelector('.md_overlay');
-const closeButton = modal.querySelector('button');
+const closeButton = modal.querySelector('.modalClose');
+const messageInput = modal.querySelector('.modalAnswer');
 const questionDeleteButton = document.getElementById('questionDelete');
+const exampleMessage = document.getElementById('messageObject1');
+const messageSendButton = document.getElementById('messageSend');
+const keywordButton = document.getElementById('btnradio1');
+const messageButton = document.getElementById('btnradio2');
+const keywordRow = document.getElementById('keywordRow');
+
 //동작함수
 const openModal = () => {
   modal.classList.remove('hidden');
@@ -15,12 +22,52 @@ const checkDelete = () => {
   const deleteResult = confirm('정말 삭제하시겠습니까?');
   if (deleteResult) {
     console.log('삭제');
+    exampleMessage.classList.add('hidden');
   } else {
     console.log('삭제안함');
   }
 };
+
+const sendMessage = () => {
+  alert('메시지가 전송되었습니다.');
+  closeModal();
+};
+
+const showKeyword = () => {
+  exampleMessage.classList.add('hidden');
+  keywordRow.classList.remove('hidden');
+};
+
+const showMessage = () => {
+  exampleMessage.classList.remove('hidden');
+  keywordRow.classList.add('hidden');
+};
+
 //클릭 이벤트
 openButton.addEventListener('click', openModal);
 closeButton.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 questionDeleteButton.addEventListener('click', checkDelete);
+messageSendButton.addEventListener('click', sendMessage);
+keywordButton.addEventListener('click', showKeyword);
+messageButton.addEventListener('click', showMessage);
+
+function is_mobile() {
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    return true;
+  }
+
+  if (typeof window.orientation !== 'undefined') {
+    return true;
+  }
+
+  var iOSios =
+    !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+  if (iOSios) return true;
+
+  return false;
+}
